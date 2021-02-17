51fc3dc3acd3a73f2bee1158f066fc296ccfc1fa
david.llamas <strokes-hadcore@hotmail.com>
2021-02-17 13:12:06 -0600

    resolving problems

--- client/ecomsurtestfront/src/App.js
+++ client/ecomsurtestfront/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route,  } from "react-router-dom"
 import Globalstate from "./components/redux/Globalstate"
 import CartComponet  from "./components/CartComponet"
 
